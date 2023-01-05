525fbf20a798913353403927c33e20746047168c
Eric Sayler <easayler@gmail.com>
2023-01-05 04:49:42 +0000

    refactor tests, fix failing test

--- hello_test.go
+++ hello_test.go
@@ -7,18 +7,20 @@ func TestHello(t *testing.T) {
 		got := Hello("Eric")
 		want := "Hello, Eric!"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World!"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
